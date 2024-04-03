1ba46c8f7b15b05c1d479a06948abeb1d1464add
Torsten Werner <torsten.werner@westernacher.com>
2024-04-03 09:39:13 +0200

    add the StsConfiguration

---
+++ src/main/java/org/example/double_it_sts/StsConfiguration.java
@@ -0,0 +1,43 @@
+package org.example.double_it_sts;
+
+import jakarta.xml.ws.Endpoint;
+import jakarta.xml.ws.soap.SOAPBinding;
+import lombok.SneakyThrows;
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.sts.StaticSTSProperties;
+import org.apache.cxf.sts.provider.DefaultSecurityTokenServiceProvider;
+import org.apache.cxf.sts.service.StaticService;
+import org.apache.cxf.ws.security.sts.provider.SecurityTokenServiceProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class StsConfiguration {
+    @SneakyThrows
+    @Bean
+    public SecurityTokenServiceProvider securityTokenServiceProvider() {
+        final var provider = new DefaultSecurityTokenServiceProvider();
+        
+        final var stsProperties = new StaticSTSProperties();
+        stsProperties.setSignatureUsername("signatureUsername");
+        stsProperties.setIssuer("issuer");
+//        stsProperties.setCallbackHandler(callbacks -> {});
+        provider.setStsProperties(stsProperties);
+        
+        final var attributeService = new StaticService();
+        attributeService.setEndpoints(List.of("http://localhost:8080/services/double-it"));
+        provider.setServices(List.of(attributeService));
+        return provider;
+    }
+
+    @Bean
+    public Endpoint sts(Bus bus, SecurityTokenServiceProvider stsProvider) {
+        final var endpoint = new EndpointImpl(bus, stsProvider);
+        endpoint.setBindingUri(SOAPBinding.SOAP12HTTP_BINDING);
+        endpoint.publish("/sts"); // http://localhost:8080/services/sts?wsdl
+        return endpoint;
+    }
+}
