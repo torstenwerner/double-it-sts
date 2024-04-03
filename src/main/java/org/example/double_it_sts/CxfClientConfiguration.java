4690f48144fdadf1a4589cfc8115de8f94bbab9a
Torsten Werner <torsten.werner@westernacher.com>
2024-04-03 15:47:31 +0200

    improve stsclient configuration

--- src/main/java/org/example/double_it_sts/CxfClientConfiguration.java
+++ src/main/java/org/example/double_it_sts/CxfClientConfiguration.java
@@ -6,6 +6,7 @@ import org.apache.cxf.ws.security.trust.STSClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.xml.namespace.QName;
 import java.util.List;
 
 import static org.apache.cxf.rt.security.SecurityConstants.*;
@@ -16,10 +17,12 @@ public class CxfClientConfiguration {
     @Bean
     public STSClient stsClient(Bus bus, CleartextLogger cleartextLogger) {
         final var stsClient = new STSClient(bus);
-        stsClient.setLocation("http://localhost:8080/services/sts");
+        stsClient.setWsdlLocation("DoubleSTSService.wsdl");
+        stsClient.setServiceQName(QName.valueOf("{http://docs.oasis-open.org/ws-sx/ws-trust/200512/}SecurityTokenService"));
+        stsClient.setEndpointQName(QName.valueOf("{http://docs.oasis-open.org/ws-sx/ws-trust/200512/}STS_Port"));
 
         stsClient.getProperties().put(USERNAME, "alice");
-        stsClient.getProperties().put(CALLBACK_HANDLER,  new ClientCallbackHandler());
+        stsClient.getProperties().put(CALLBACK_HANDLER, new ClientCallbackHandler());
         stsClient.getProperties().put(ENCRYPT_PROPERTIES, "clientKeystore.properties");
         stsClient.getProperties().put(ENCRYPT_USERNAME, "client");
         stsClient.getProperties().put(STS_TOKEN_PROPERTIES, "clientKeystore.properties");
